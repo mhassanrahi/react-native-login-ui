b75260258529f98aff1a904353c8f4618692f003
Mehmood <m.hassan.rahi@gmail.com>
2021-09-01 23:59:43 +0500

    * Updated App component

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {Button} from 'react-native-paper';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* logo */}
+      <View style={styles.logoContainer}>
+        <Image source={require('./src/assets/icons/logo.png')} />
+        <Text style={styles.logoText}>Productbox</Text>
+      </View>
+
+      {/* social buttons */}
+      <View style={styles.socialButtonsContainer}>
+        <Button
+          icon="camera"
+          mode="contained"
+          onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -13,7 +32,24 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
+    marginHorizontal: wp('10'),
+    backgroundColor: 'red',
   },
+
+  logoContainer: {
+    height: hp(20),
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  logoText: {
+    marginTop: hp(1),
+    fontSize: wp(5),
+    fontWeight: '500',
+    color: 'blue',
+  },
+
+  socialButtonsContainer: {},
 });
